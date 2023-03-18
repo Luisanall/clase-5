5f002cb64b9191cc8dac6d801e2ea28469747564
Luisanall <127669786+Luisanall@users.noreply.github.com>
2023-03-18 12:02:19 -0300

    clase producto

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import class Producto{}
+    public class main{
+    public static void main(String[] args) {
+        ;
+        public class Producto {
+
+        System.out.println("la cantidad de la gaseosa "+cocacola.cantidad);
+        System.out.println(" la  marca de la gaseosa "+cocacola.marca);
+        System.out.println("el valor de la gaseosa es de  =  "+cocacola.valor);
+        }
+    }
+
+}
---
+++ src/Producto.java
@@ -0,0 +1,15 @@
+public class Producto {
+    String marca;
+    int cantidad;
+    int valor;
+
+    public static void main (String [] arg){
+        Producto cocacola = new Producto ();
+cocacola.cantidad = 1;
+cocacola.marca = "Coca cola";
+cocacola.valor = 300;
+        
+
+    }
+
+}
